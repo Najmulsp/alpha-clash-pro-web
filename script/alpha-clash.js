aaeeecb44ed218e3d9726fe9dc1bf26ac6db1041
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-02-16 07:57:36 +0600

    add and remove classlist

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -1,3 +1,15 @@
+/*function play(){
+// step-1: after click the button the home screen will be hidden by adding classList hidden.
+const homeSection =document.getElementById('home-screen');
+homeSection.classList.add('hidden');
+
+// step-2: show the playground section  by adding remove classList hidden.
+const playgroundSection =document.getElementById('play-ground');
+playgroundSection.classList.remove('hidden');
+}*/
+
 function play(){
-    console.log('btn click hocche')
+    hideElementById('home-screen');
+    showElementById('play-ground');
 }
+
